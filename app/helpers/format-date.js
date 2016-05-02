89348e74c5c13ada3f9fc57e7aff66251a8cd449
Anne Kao <annekao94@gmail.com>
2016-05-02 01:27:06 -0700

    fix last error

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
-export function formatDate(date) {
-  var date = new Date(date);
+export function formatDate(uglyDate) {
+  var date = new Date(uglyDate);
   var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   return days[date.getDay()] + ', ' + months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
